41c9f3a6f4c7295a2e47620d947b0dbaccf71bb1
GiaFil7 <GiaFil007@gmail.com>
2018-09-11 18:14:00 +0300

    Refactor events

--- events.js
+++ events.js
@@ -1,3 +1,35 @@
+function setupDiv (item, itemCount, inputType) {
+  let itemDiv = document.createElement('div')
+  itemDiv.setAttribute('id', item + '-' + itemCount)
+  itemDiv.setAttribute('class', 'addedElements')
+  let itemToAdd = document.getElementById(inputType).value
+  return { itemDiv, itemToAdd }
+}
+
+function setupCheckbox (item, itemCount) {
+  let itemCheckbox = document.createElement('input')
+  itemCheckbox.setAttribute('type', 'checkbox')
+  let itemLabel = document.createElement('label')
+  itemCheckbox.setAttribute('id', item + 'Label-' + itemCount)
+  itemCheckbox.setAttribute('name', item)
+  itemLabel.setAttribute('for', item + 'Label-' + itemCount)
+  return { itemCheckbox, itemLabel }
+}
+
+function setupButtons () {
+  let itemEditButton = document.createElement('button')
+  itemEditButton.setAttribute('id', 'edit')
+  itemEditButton.setAttribute('class', 'btn')
+  itemEditButton.setAttribute('onClick', 'editItem(this.parentElement, this)')
+  itemEditButton.innerHTML = 'Edit'
+  let itemDeleteButton = document.createElement('button')
+  itemDeleteButton.setAttribute('id', 'delete')
+  itemDeleteButton.setAttribute('class', 'btn')
+  itemDeleteButton.setAttribute('onClick', 'deleteItem(this.parentElement)')
+  itemDeleteButton.innerHTML = 'Delete'
+  return { itemEditButton, itemDeleteButton }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   let eventCount = 1
   let goalCount = 1
@@ -5,36 +37,16 @@ document.addEventListener('DOMContentLoaded', function () {
   function addItem (itemCount, inputType, section, item) {
     if (document.getElementById(inputType).value) {
       let type = document.getElementById(section)
-      // Setup of div
-      let itemDiv = document.createElement('div')
-      itemDiv.setAttribute('id', item + '-' + itemCount)
-      itemDiv.setAttribute('class', 'addedElements')
-      let itemToAdd = document.getElementById(inputType).value
-      // Setup of checkbox
-      let itemCheckbox = document.createElement('input')
-      itemCheckbox.setAttribute('type', 'checkbox')
-      let itemLabel = document.createElement('label')
-      itemCheckbox.setAttribute('id', item + 'Label-' + itemCount)
-      itemCheckbox.setAttribute('name', item)
-      itemLabel.setAttribute('for', item + 'Label-' + itemCount)
-      // Setup of buttons
-      let itemEditButton = document.createElement('button')
-      itemEditButton.setAttribute('id', 'edit')
-      itemEditButton.setAttribute('class', 'btn')
-      itemEditButton.setAttribute('onClick', 'editItem(this.parentElement, this)')
-      itemEditButton.innerHTML = 'Edit'
-      let itemDeleteButton = document.createElement('button')
-      itemDeleteButton.setAttribute('id', 'delete')
-      itemDeleteButton.setAttribute('class', 'btn')
-      itemDeleteButton.setAttribute('onClick', 'deleteItem(this.parentElement)')
-      itemDeleteButton.innerHTML = 'Delete'
+      const divVars = setupDiv(item, itemCount, inputType)
+      const checkboxVars = setupCheckbox(item, itemCount)
+      const buttonVars = setupButtons()
       // Setup of page structure
-      type.appendChild(itemDiv)
-      itemDiv.appendChild(itemCheckbox)
-      itemDiv.appendChild(itemLabel)
-      itemLabel.innerHTML = itemToAdd
-      itemDiv.appendChild(itemEditButton)
-      itemDiv.appendChild(itemDeleteButton)
+      type.appendChild(divVars.itemDiv)
+      divVars.itemDiv.appendChild(checkboxVars.itemCheckbox)
+      divVars.itemDiv.appendChild(checkboxVars.itemLabel)
+      checkboxVars.itemLabel.innerHTML = divVars.itemToAdd
+      divVars.itemDiv.appendChild(buttonVars.itemEditButton)
+      divVars.itemDiv.appendChild(buttonVars.itemDeleteButton)
     }
     document.getElementById(inputType).value = ''
   }
